4f9154a45ae988e028e23a74e7a06d177a142581
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2025-02-15 19:34:28 -0500

    add skeleton

--- lox.py
+++ lox.py
@@ -3,6 +3,23 @@ import argparse
 def main():
     # if there is an arg read from filename
     # else open interpreter
+    parser = argparse.ArgumentParser(
+                    prog='Lox',
+                    description='Usage: jlox [script]',
+                    )
+    parser.add_argument('-f', '--filename', required=False) 
+    args = parser.parse_args()
+    if args.filename:
+        print(args.filename)
+        runFile()
+    else:
+        runPrompt()
+
+def runFile():
+    pass
+
+
+def runPrompt():
     pass
 
 
