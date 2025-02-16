230499ab8b155b8667ace99edc24fe011c1a2f87
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2025-02-15 20:18:16 -0500

    add token class

---
+++ src/token.py
@@ -0,0 +1,9 @@
+class Token():
+    def __init__(self,type:str,lexeme:str, literal:str, line:int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __repr__(self) -> str:
+        return f"{self.type} {self.lexeme} {self.literal}"
